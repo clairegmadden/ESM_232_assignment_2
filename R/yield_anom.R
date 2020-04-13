ed5a8d36b88bfc5b40ae6a8538b6bea14b87a8aa
Claire Madden <clairegmadden@gmail.com>
2020-04-13 15:22:05 -0700

    wrote out function parameters and general function from paper

--- R/yield_anom.R
+++ R/yield_anom.R
@@ -1,3 +1,13 @@
 
 
+# set up function to calculate almond yield anomaly
+
+# define variables and parameters
+# tn is minumum temperature (t) of the month(n)
+# tx is the maximum temperature (t) of the month(x)
+# p is precip
+
+# function in paper : Y = -0.015tn,2 - 0.0046t(^2)n,2 - 0.07P1 + 0.0043P(^2)1 +0.28
+
+
 
