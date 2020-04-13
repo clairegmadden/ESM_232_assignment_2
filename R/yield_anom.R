c98807a6032323f0e7de7eb275d86b929e870e1a
Claire Madden <clairegmadden@gmail.com>
2020-04-13 16:27:11 -0700

    start on function and markdown!

--- R/yield_anom.R
+++ R/yield_anom.R
@@ -10,4 +10,16 @@
 # function in paper : Y = -0.015tn,2 - 0.0046t(^2)n,2 - 0.07P1 + 0.0043P(^2)1 +0.28
 
 
+yield_anom = function(tn1 = -0.015, tn2 = -0.0046, p1 = -0.07, p2 = 0.0043, int = 0.28, min_month_temp, precip){
+  
+  yield_anom = (tn1*min_month_temp) + (tn2*(min_month_temp^2)) + (p1*precip) + (p2*(precip^2))+ int
+  
+  return(yield_anom)
+  
+}
+
+
+
+
+
 
