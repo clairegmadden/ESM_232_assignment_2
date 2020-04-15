9b28a63471ceddbdcf6423e3ad93653b04d723de
Claire Madden <clairegmadden@gmail.com>
2020-04-15 09:04:37 -0700

    notes from wednesday class

--- R/yield_anom.R
+++ R/yield_anom.R
@@ -12,14 +12,23 @@
 
 yield_anom = function(tn1 = -0.015, tn2 = -0.0046, p1 = -0.07, p2 = 0.0043, int = 0.28, min_month_temp, precip){
   
+  # solve for yield
   yield_anom = (tn1*min_month_temp) + (tn2*(min_month_temp^2)) + (p1*precip) + (p2*(precip^2))+ int
   
-  return(yield_anom)
+  
+  # solve for max
+  max = 
+  
+  # solve for min 
+  min = 
+  
+  
+  return(list(yield_anom, max, min))
   
 }
 
 
 
-
+# to plot yield anom, you can make the first part of the output into a dataframe to plot
 
 
