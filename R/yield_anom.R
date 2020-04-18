8f35717da45475e3aa9eb354d4b9f3348da010e9
Lydia Bleifuss <lbleifuss@bren.ucsb.edu>
2020-04-17 18:18:43 -0700

    too funky to function

--- R/yield_anom.R
+++ R/yield_anom.R
@@ -16,10 +16,10 @@ yield_anom = function(tn1 = -0.015, tn2 = -0.0046, p1 = -0.07, p2 = 0.0043, int 
   
   clim_monthly <- clim_data %>% 
   group_by(month, year) %>% 
-  summarize(meantmax = mean(tmin_c), precip=sum(precip)) #maybe check if this is supposed to be mean rather than sum (slack notes)  
+  summarize(meantmin = mean(tmin_c), precip=sum(precip)) #maybe check if this is supposed to be mean rather than sum (slack notes)  
   
   precip_2 <- clim_monthly %>% 
-  select(!meantmax) %>% 
+  select(!meantmin) %>% 
   filter(month == 2)
   
   temp_1 <- clim_monthly %>% 
@@ -30,27 +30,18 @@ yield_anom = function(tn1 = -0.015, tn2 = -0.0046, p1 = -0.07, p2 = 0.0043, int 
   
   for(i in 1:nrow(df_anom)) {
     
-  df_anom$y_anom[i] <- tn1*temp_1$meantmax[i] + tn2*temp_1$meantmax^2[i] + p1*precip_2$precip[i] + p2*precip_2$precip^2[i] + int
+  df_anom$y_anom[i] <- tn1*temp_1$meantmin[i] + tn2*temp_1$meantmin[i]^2 + p1*precip_2$precip[i] + p2*precip_2$precip[i]^2 + int
     
   }
   
+  y_anom_yr <- select(df_anom, c(year,y_anom))
   
-  return((df_anom))
+  
+  return(list(y_anom_yr))
   
 }
 
 
-
-
-# solve for yield
-yield_anom = (tn1*min_month_temp) + (tn2*(min_month_temp^2)) + (p1*precip) + (p2*(precip^2))+ int
-
-# solve for max
-max = 
-  
-  # solve for min 
-  min = 
-  
 
 
 # to plot yield anom, you can make the first part of the output into a dataframe to plot
