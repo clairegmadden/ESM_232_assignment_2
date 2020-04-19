194d9a220be98b9ab186e15b8fa7fba3c51979e0
Claire Madden <clairegmadden@gmail.com>
2020-04-19 10:11:34 -0700

    got the function working woop wooooop

--- R/yield_anom.R
+++ R/yield_anom.R
@@ -12,33 +12,40 @@
 # function in paper : Y = -0.015tn,2 - 0.0046t(^2)n,2 - 0.07P1 + 0.0043P(^2)1 +0.28
 
 
-yield_anom = function(tn1 = -0.015, tn2 = -0.0046, p1 = -0.07, p2 = 0.0043, int = 0.28, clim_data){
+yield_anom = function(tn1 = -0.015, tn2 = -0.0046, p1 = -0.07, p2 = 0.0043, int = 0.28, clim_data) {
   
   clim_monthly <- clim_data %>% 
   group_by(month, year) %>% 
-  summarize(meantmin = mean(tmin_c), precip=sum(precip)) %>%  #maybe check if this is supposed to be mean rather than sum (slack notes)  
+  summarize(meantmin = mean(tmin_c), 
+            precip=sum(precip)) %>%  #maybe check if this is supposed to be mean rather than sum (slack notes)  
     ungroup()
   
   precip_2 <- clim_monthly %>% 
-  select(!meantmin) %>% 
+  select(-meantmin) %>% 
   filter(month == 2)
   
   temp_1 <- clim_monthly %>% 
-  select(!precip) %>% 
+  select(-precip) %>% 
   filter(month == 1)
     
   df_anom <- data.frame(year = temp_1$year, y_anom = NA) 
   
-  for(i in 1:length(df_anom)) {
+  for(i in 1:nrow(df_anom)) {
     
-  y_anom = tn1*temp_1$meantmin[i] + tn2*temp_1$meantmin[i]^2 + p1*precip_2$precip[i] + p2*precip_2$precip[i]^2 + int
+  df_anom$y_anom[i] <- tn1*temp_1$meantmin[i] + 
+    tn2*temp_1$meantmin[i]^2 + 
+    p1*precip_2$precip[i] + 
+    p2*precip_2$precip[i]^2 + 
+    int
     
   }
   
-  y_anom_yr <- select(df_anom, c(year,y_anom))
+  y_anom_yr <- dplyr::select(df_anom, c(year,y_anom))
   
+  min_anom <- min(y_anom_yr$y_anom)
+  max_anom <- max(y_anom_yr$y_anom)
   
-  return(y_anom_yr)
+  return(list(y_anom_yr, min_anom, max_anom))
   
 }
 
